8871a8e2fc3b8abb83e8f94d8e70947f65c057d5
No NOréo <70480609+Non0reo@users.noreply.github.com>
2024-09-20 11:03:45 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,22 +5,11 @@ const space = {
   y: 100,
 }
 let img;
-let colors = []
-
-function preload() {
-  img = loadImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDFfgBG1223zZsAM6ZU4p1hMN9J2svxM3-ww&s')
-}
 
 function setup() {
   createCanvas((SQUARE_COUNT + 1) * space.x, (SQUARE_COUNT + 1) * space.y);
   background(250);
   strokeWeight(6);
-  colorMode(HSL);
-  //noLoop();
-
-  // for (let i = 0; i < SQUARE_COUNT; i++) {
-  //   colors.push(color(random(360), 25, 70, 1.0))    
-  // }
 }
 
 function draw() {
@@ -29,8 +18,7 @@ function draw() {
   let rotationCooef = map(mouseX, 0, width, -0.05, 0.05);
   //0.01, 0.03, 0.07
 
-  for (let x = 0; x < SQUARE_COUNT; x++) {
-
+  for (let x = 0; x < SQUARE_COUNT; x++) 
     for (let y = 0; y < SQUARE_COUNT; y++) {
       
       push()
